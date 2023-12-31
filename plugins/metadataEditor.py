027c70cbf1cb490f172636bade6bebf4caff86d6
Jot43 <139986366+Jot43@users.noreply.github.com>
2023-12-31 13:42:57 +0530

    Update metadataEditor.py

--- plugins/metadataEditor.py
+++ plugins/metadataEditor.py
@@ -1,14 +1,4 @@
-#
-# (c) Yash Oswal | yashoswal18@gmail.com
-
-from pyrogram import Client
-from pyrogram.types import Message
-from bot import mergeApp, LOGGER
-from config import Config
-
-async def metaEditor(c:Client, m: Message):
-    1
-    import
+    import cv2
 
 def extract_video_metadata(video_path):
     """
